0abcdae15a6d35116b27ce0b6175da8443c09b54
divya <divyapatange0@gmail.com>
2020-10-22 16:22:07 +0530

    login register performed

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::get('user-profile', 'App\Http\Controllers\AuthController@userProfile');
+});
